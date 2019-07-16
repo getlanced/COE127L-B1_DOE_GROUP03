66aef9b564b691e893d9871ea2a1784a675bb77a
getlanced <henrymc381@gmail.com>
2019-07-16 22:53:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,92 @@
-// writing on a text file
+
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <map>
 using namespace std;
-void comp(string,string,long*);
 int main ()
 {
-    ifstream myfile ("words.txt");
-    string line;
-    long num_count = 0 ;
+    ifstream myfile ("parawords.txt");
+    string line,subStr;
+    string subStr_comp = "he";
+    long OccCount, LnCount;
+    OccCount = LnCount = 0;
+
+	map<int,string> mymap;
+
+	//Load to Map
     if (myfile.is_open())
     {
         while(!myfile.eof())
         {
             getline(myfile,line);
-            comp(line,"ea",&num_count);
+            if(line == "\n")
+                continue;
+            else
+            {
+                //Split string by space
+                //Remove Trailing Space
+                for(int i=int(line.size()-1);i >= 0 ;i-- )
+                {
+                    if(line[line.size() - 1] == '\n')
+                        continue;
+                    else if(line[i] == ' ')
+                        line.erase(i,1);
+                    else
+                        break;
+                }
+                //Remove leading Space
+                for(int i=0;i<int(line.size());i++ )
+                {
+                    if(line[i] == ' ')
+                        line.erase(i,1);
+                    else
+                        break;
+                }
+                //Map Filling
+                for(int i = 0; i< int(line.size()); i++)
+                {
+                    if(line[i] == ' ')
+                    {
+                        i++;
+                        mymap.insert(pair<int,string>(LnCount,subStr));
+                        subStr.clear();
+                        LnCount++;
+                    }
+                    else
+                        subStr.push_back(line[i]);
+                }
+            }
         }
     }
     else
         cout << "Unable to open file";
 
-    cout<<"Occurrences: "<<num_count<<endl;
     myfile.close();
 
-    return 0;
-}
-
-void comp(string line,string sub,long* c)
-{
-    int j =0;
-    for(int i = 0; i<int(line.length());i++)
+	//Parallelize This
+    //#pragma omp parallel for default(shared) reduction(+:OccCount) //Error
+    for(auto it = mymap.begin();it != mymap.end();it++)
     {
-        while(line[i] == sub[j])
+        int j =0;
+        line = it->second;
+        for(int i=0; i<int(line.size());i++)
         {
-            if(j == int(sub.length()) - 1)
+            while(line[i] == subStr_comp[j])
             {
-                (*c)++;
-                break;
+                if(j == int(subStr_comp.length()) - 1)
+                {
+                    //Critical region that manipulates OccCount
+                    OccCount++;
+                    break;
+                }
+                i++;
+                j++;
             }
-            i++;
-            j++;
+            j=0;
         }
-        j=0;
     }
+    cout<<OccCount<<endl;
+    mymap.clear();//Clear Map, check for next file
+    return 0;
 }
