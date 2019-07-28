9d9ea436b2738cd21959546ccc35297b1c095109
ccardsss <52280243+ccardsss@users.noreply.github.com>
2019-07-28 19:33:58 +0800

    Update graphtest.py

--- graphtest.py
+++ graphtest.py
@@ -1,18 +1,31 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import csv
 
+data = []
+with open('graphDat.csv','r') as f:
+    reader = csv.reader(f,delimiter= ',')
+    for line in reader:
+        data.append(line)
 
-#set directory
-df=pd.read_excel('graphdata.xlsx','Sheet1')
+data1 = []
+for i in range(1,len(data)):
+    data1.append(data[i][1])
+
+data2 = []
+for i in range (1,len(data)):
+    data2.append(data[i][2])
+
+data3 = []    
+for i in range (1,len(data)):
+    data3.append(data[i][3])
+
 #set plot
-plt.bar(df['Time Take: Processing'],df['Time Taken: Map Fill'])
+plt.plot(data2,data1,'.r-',label ='Setup Time')
 
+plt.plot(data2,data3,'.b-',label = 'Performance Time')
 
 #set label
-plt.xlabel('Time Taken: Map Fill')
-plt.ylabel('Time Take: Processing')
-plt.title('Time Take: Processing vs Time Taken: Map Fill Graph')
-
-
+plt.xlabel('No. of Lines')
+plt.ylabel('Time Taken (s)')
+plt.legend()
 plt.show()
-
