3b789b159d5dc8ce4b25905506137bcd97d198f6
loisborrega <50916950+loisborrega@users.noreply.github.com>
2019-07-28 14:45:45 +0800

    main.cpp update
    -added iteration of files in a designated folder

--- main.cpp
+++ main.cpp
@@ -2,202 +2,139 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
 #include <omp.h>
+#include <dirent.h>
+
 using namespace std;
 int main()
 {
-	ifstream myfile("words.txt");
-	string line, subStr = "";
-	string subStr_comp = "";
-	int occCount, map_key = 0;
-
-	map<int,string> mymap;
-	cout<< "Enter string to compare: ";
-	cin >> subStr_comp;
-	
-	double vtime_start = omp_get_wtime();
-	//Load to vector
-	if (myfile.is_open())
-	{
-		while (!myfile.eof())
-		{
-			getline(myfile, line);
-			if (line == "\n")
-				continue;
-			//Get Line and remove leading and trailing spaces
-			else
-			{
-				//Remove Trailing Space of line
-				for (int i = int(line.size() - 1); i >= 0; i--)
-				{
-					if (line[line.size() - 1] == '\n')
-						continue;
-					else if (line[i] == ' ')
-						line.erase(i);
-					else
-						break;
-				}
-				//Remove leading Space of line
-				for (int i = 0; i<int(line.size()); i++)
-				{
-					if (line[i] == ' ')
-						line.erase(i);
-					else
-						break;
-				}
-				//Map Fill Handler
-				for (int i = 0; i< int(line.size()); i++)
-				{
-					if (line[i] == ' ')
-					{	//Entries with multiple spaces per word
-						//Peek for frontal spaces and ignores it
-						if (line[i + 1] == ' ')
-							continue;
-						else
-						{
-							mymap.insert(pair<int, string>(map_key, subStr));
-							subStr.clear();
-						}
-					}
-					else if (i == int(line.size() - 1))
-					{
-						//For spaceless entries
-						subStr.push_back(line[i]);
-						mymap.insert(pair<int,string>(map_key,subStr));
-						subStr.clear();
-					}
-					else
-						subStr.push_back(line[i]);
-				}
-			}
-		}
-	}
-	else
-	{
-		cout << "Unable to open file\n";
-		return -1;
-	}
-
-	myfile.close();
-	double vEndTime = omp_get_wtime() - vtime_start;
-	cout << "Vector Filling Time Taken: " << vEndTime << endl;
-
-	//Parallelize This
-	double start_time = omp_get_wtime();
-	int j = 0;
-	int k = 0;
-	#pragma omp parallel for private(line,k,j)
-	for (int i = 0; i < int(mymap.size()); i++)
-	{
-		j = 0;
-		line = mymap[i];
-		for (k = 0; k<int(line.size()); k++)
-		{
-			while (line[k] == subStr_comp[j])
-			{
-				if (j == int(subStr_comp.size()) - 1)
-				{
-					//Critical region that manipulates occCount
-					#pragma omp critical
-					occCount++;
-					break;
-				}
-				k++;
-				j++;
-			}
-			j = 0;
-		}
-	}
-	double time = omp_get_wtime() - start_time;
-	cout << "Text Processing Time Taken: " << time << endl;
-	cout << "Occurrence of " << subStr_comp << " : " << occCount << endl;
-	mymap.clear();//Clear list, check for next file
-	return 0;
-=======
-int main ()
-{
-    ifstream myfile ("words.txt");
-    string line,subStr;
-    string subStr_comp = "he"; //Will Change to user input
-    long OccCount, LnCount;
-    OccCount = LnCount = 0;
-
-	map<int,string> mymap;
-
-	//Load to Map
-    if (myfile.is_open())
+    DIR *dpdf;
+    struct dirent *epdf;
+    vector<string> path;
+    dpdf = opendir("./txtFiles");
+    if (dpdf != NULL)
     {
-        while(!myfile.eof())
-        {
-            getline(myfile,line);
-            if(line == "\n")
-                continue;
+       while (epdf = readdir(dpdf))
+       {
+           string subStr_file_ext = ".txt";
+           if(string(epdf->d_name).find(subStr_file_ext) != string::npos)
+           {
+               string subStr_txtDir = "./txtFiles/";
+               path.push_back(subStr_txtDir + epdf->d_name);
+           }
             else
-            {
-                //Split string by space
-                //Remove Trailing Space
-                for(int i=int(line.size()-1);i >= 0 ;i-- )
-                {
-                    if(line[line.size() - 1] == '\n')
-                        continue;
-                    else if(line[i] == ' ')
-                        line.erase(i,1);
-                    else
-                        break;
-                }
-                //Remove leading Space
-                for(int i=0;i<int(line.size());i++ )
-                {
-                    if(line[i] == ' ')
-                        line.erase(i,1);
-                    else
-                        break;
-                }
-                //Map Filling
-                for(int i = 0; i< int(line.size()); i++)
-                {
-                    if(line[i] == ' ')
-                    {
-                        i++;
-                        mymap.insert(pair<int,string>(LnCount,subStr));
-                        subStr.clear();
-                        LnCount++;
-                    }
-                    else
-                        subStr.push_back(line[i]);
-                }
-            }
-        }
+                continue;
+       }
     }
-    else
-        cout << "Unable to open file";
-
-    myfile.close();
-
-	//Parallelize This
-    //#pragma omp parallel for default(shared) reduction(+:OccCount) //Error
-    for(auto it = mymap.begin();it != mymap.end();it++)
+    closedir(dpdf);
+    string subStr_comp = "";
+    cout << "Enter string to compare: ";
+    cin >> subStr_comp;
+    for(auto it = path.begin();it != path.end(); it++)
     {
-        int j =0;
-        line = it->second;
-        for(int i=0; i<int(line.size());i++)
+        ifstream myfile(*it);
+        string line = "", subStr = "";
+        int occCount = 0, map_key = 0;
+        map<int, string> mymap;
+        double fill_time_start = omp_get_wtime();
+        //Load to map
+        if (myfile.is_open())
         {
-            while(line[i] == subStr_comp[j])
+            while (!myfile.eof())
             {
-                if(j == int(subStr_comp.length()) - 1)
+                getline(myfile, line);
+                if (line == "\n")
+                    continue;
+                //Get Line and remove leading and trailing spaces
+                else
                 {
-                    //Critical region that manipulates OccCount
-                    OccCount++;
-                    break;
+                    //Remove Trailing Space of line
+                    for (int i = int(line.size() - 1); i >= 0; i--)
+                    {
+                        if (line[line.size() - 1] == '\n')
+                            continue;
+                        else if (line[i] == ' ')
+                            line.erase(i);
+                        else
+                            break;
+                    }
+                    //Remove leading Space of line
+                    for (int i = 0; i<int(line.size()); i++)
+                    {
+                        if (line[i] == ' ')
+                            line.erase(i);
+                        else
+                            break;
+                    }
+                    //Map Fill Handler
+                    for (int i = 0; i< int(line.size()); i++)
+                    {
+                        if (line[i] == ' ')
+                        {	
+			//Entries with multiple spaces per word
+                            //Peek for frontal spaces and ignores it
+                            if (line[i + 1] == ' ')
+                                continue;
+                            else
+                            {
+                                mymap.insert(pair<int, string>(map_key, subStr));
+                                map_key++;
+                                subStr.clear();
+                            }
+                        }
+                        else if (i == int(line.size() - 1))
+                        {
+                            //For spaceless entries
+                            subStr.push_back(line[i]);
+                            mymap.insert(pair<int, string>(map_key, subStr));
+                            map_key++;
+                            subStr.clear();
+                        }
+                        else
+                            subStr.push_back(line[i]);
+                    }
                 }
-                i++;
-                j++;
             }
-            j=0;
         }
+        else
+        {
+            cout << "Unable to open file\n";
+            return -1;
+        }
+        myfile.close();
+        double fill_EndTime = omp_get_wtime() - fill_time_start;
+        cout << "Map Filling Time Taken: " << fill_EndTime << endl;
+        //Parallelize This
+        double proc_start_time = omp_get_wtime();
+        int j = 0;
+        int k = 0;
+    #pragma omp parallel for private(line,k,j)
+        for (int i = 0; i < int(mymap.size()); i++)
+        {
+            j = 0;
+            line = mymap[i];
+            for (k = 0; k<int(line.size()); k++)
+            {
+                while (line[k] == subStr_comp[j])
+                {
+                    if (j == int(subStr_comp.size()) - 1)
+                    {
+                        //Critical region that manipulates occCount
+                        #pragma omp critical
+                        occCount++;
+                        break;
+                    }
+                    k++;
+                    j++;
+                }
+                j = 0;
+            }
+        }
+        double proc_end_time = omp_get_wtime() - proc_start_time;
+        cout << "Text Processing Time Taken: " << proc_end_time << endl;
+        cout << "Occurrence of " << subStr_comp << " : " << occCount << endl;
+        mymap.clear();//Clear list, check for next file
     }
-    cout<<OccCount<<endl;
-    //Implement folder iteration
-    mymap.clear();//Clear Map, check for next file
-    return 0;
+	return 0;
 }
